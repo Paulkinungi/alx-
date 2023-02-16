74fdbc33d7481def6f6d85f09dd3982dfa0e87f5
Kinungipaul <kinungipaul21@gmail.com>
2023-02-16 23:10:09 +0300

    alx-low_level_programming

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,10 @@
+#include <std.h
+/**
+ * main -A programme printing a line with puts function
+ * Return: 0 (completes)
+ */
+int main(void)
+{
+    puts("\"Programming is like building a multilingual puzzle");
+    return(0)
+}
